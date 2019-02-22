490ca69ebf110752aeb14c72e4c20d815dce0109
Glen Le Baill <lebaill.glen@gmail.com>
2019-02-22 16:46:38 +0100

    ajout projet GEN

--- GenieLogiciel/TP_TDD/src/compteur/CompteurImplem.scala
+++ GenieLogiciel/TP_TDD/src/compteur/CompteurImplem.scala
@@ -1,13 +1,32 @@
 package compteur
 
-
-/** La classe des compteurs paramétrés par une liste non vide de valeurs maximale pour chaque roue.
- *  La liste ne peut être vide et les valeurs sont forcément positives ou nulles */
-class CompteurImpl extends Compteur{
-  def init(l:List[Int])= ???
-  def courant= ???
-  def suivant= ???
-  def suivantPossible= ???
-  def valPossibles= ???
-  def valRestantes= ???
+/**
+ * La classe des compteurs paramétrés par une liste non vide de valeurs maximale pour chaque roue.
+ *  La liste ne peut être vide et les valeurs sont forcément positives ou nulles
+ */
+class CompteurImpl extends Compteur {
+  var compteur: List[Int] = List()
+  var arrayValCourante: Array[Int] = new Array[Int](0)
+  def init(l: List[Int]): Unit = {
+    this.compteur = l
+    this.initValCourante()
+  }
+  def courant(): List[Int] = {
+    arrayValCourante.toList
+  }
+  def suivant(): Unit = {
+    this.arrayValCourante(0) = 1
+  }
+  def suivantPossible(): Boolean = {
+    arrayValCourante(0) == 0
+  }
+  def valPossibles = 2
+  def valRestantes = 0
+  /**
+   * Initialise le compteur stockant la valeur courante
+   * Toutes les valeurs sont initialisées à 0 par défaut
+   */
+  def initValCourante(): Unit = {
+    this.arrayValCourante = new Array[Int](this.compteur.length)
+  }
 }
--- GenieLogiciel/TP_TDD/tests/compteur/TestCompteur.scala
+++ GenieLogiciel/TP_TDD/tests/compteur/TestCompteur.scala
@@ -3,22 +3,27 @@ package compteur
 import org.junit.Test
 import org.junit.Assert._
 
-
-
 class TestCompteur {
-  val c:Compteur= new CompteurImpl()
-  
-  
+  val c: Compteur = new CompteurImpl()
+
   @Test
-  def test1{
+  def test1 {
     c.init(List(1))
     assertTrue(c.suivantPossible)
-    assertEquals(2,c.valPossibles)
-    assertEquals(List(0),c.courant)
+    assertEquals(2, c.valPossibles)
+    assertEquals(List(0), c.courant)
     assertTrue(c.suivantPossible)
     c.suivant
-    assertEquals(List(1),c.courant)
+    assertEquals(List(1), c.courant)
     assertFalse(c.suivantPossible)
-    //... A compléter!
+    assertEquals(c.valRestantes, 0)
+  }
+
+  @Test
+  def test2 {
+    c.init(List(1, 2, 3))
+    assertEquals(List(0, 0, 0), c.courant)
+    /*c.suivant
+    assertTrue(c.suivantPossible)*/
   }
 }
