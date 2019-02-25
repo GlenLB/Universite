e49cbc77266d921051cc284985e00a3af3efabbb
Glen Le Baill <lebaill.glen@gmail.com>
2019-02-25 22:49:42 +0100

    Réalisation du projet TDD

--- GenieLogiciel/TP_TDD/src/compteur/CompteurImplem.scala
+++ GenieLogiciel/TP_TDD/src/compteur/CompteurImplem.scala
@@ -1,32 +1,105 @@
 package compteur
 
 /**
- * La classe des compteurs paramétrés par une liste non vide de valeurs maximale pour chaque roue.
- *  La liste ne peut être vide et les valeurs sont forcément positives ou nulles
+ * Un Compteur simule un compteur mécanique à n roues.
+ * Chaque roue a un range qui peut être différent de celui des autres roues du compteur.
+ * Exemple:  List(1, 100, 4) représente un compteur à 3 roues. La 1ère a comme valeurs possibles
+ * 0 et 1, la 2ème de 0 à 100, la 3ème de 0 à 4.
+ * L'ensemble des valeurs possibles de chaque roue est un sous-ensemble des entiers naturels commençant à 0 (0 à +inf, pas de valeurs négatives).
+ * Comme dans les compteurs mécaniques, lorsqu’une roue aura fait un tour complet,
+ * elle fera avancer d’un cran la roue située à sa gauche (si elle existe et que sa valeur n'est pas à sa valeur maximale).
+ * La liste représentant un compteur ne peut pas être vide.
+ * Un compteur ne peut que s'incrémenter, pas se décrémenter.
+ * On supposera qu’un compteur, s’il n’est pas initialisé avec init, ne comporte qu’une roue avec une seule valeur (0).
  */
 class CompteurImpl extends Compteur {
-  var compteur: List[Int] = List()
-  var arrayValCourante: Array[Int] = new Array[Int](0)
-  def init(l: List[Int]): Unit = {
-    this.compteur = l
-    this.initValCourante()
-  }
-  def courant(): List[Int] = {
-    arrayValCourante.toList
-  }
-  def suivant(): Unit = {
-    this.arrayValCourante(0) = 1
-  }
-  def suivantPossible(): Boolean = {
-    arrayValCourante(0) == 0
-  }
-  def valPossibles = 2
-  def valRestantes = 0
-  /**
-   * Initialise le compteur stockant la valeur courante
-   * Toutes les valeurs sont initialisées à 0 par défaut
-   */
-  def initValCourante(): Unit = {
-    this.arrayValCourante = new Array[Int](this.compteur.length)
-  }
+    // Représente le compteur, les valeurs maximales de chaque roue du compteur
+    private var compteur: List[Int] = null
+    // Représente la valeur courante du compteur pour chaque roue
+    private var compteurCourant: Array[Int] = null
+    // Stocke le nombre de valeurs restantes possibles
+    private var compteurValRestantes: Int = 0
+
+    def init(l: List[Int]): Unit = {
+        // La méthode n'accepte pas de liste vide ni de liste comprenant des valeurs négatives
+        // En cas de liste vide ou comprenant des valeurs négatives, ne fait rien et laisse les initialisations à null
+        if (l != Nil) {
+            for (elem <- l) {
+                if (elem < 0) return
+            }
+            compteur = l
+            // Toutes les valeurs sont initialisées à 0 par défaut
+            compteurCourant = new Array[Int](this.compteur.length)
+            // Initialise le compteur du nombre valeurs restantes possibles
+            compteurValRestantes = valPossibles() - 1
+        }
+    }
+
+    def courant(): List[Int] = {
+        if (compteur == null) {
+            null
+        } else {
+            compteurCourant.toList
+        }
+    }
+
+    def suivant(): Unit = {
+        // Si les deux compteurs sont identiques,
+        // c'est que la valeur max est atteinte.
+        if (compteur != null && compteurCourant != null && compteurCourant.toList != compteur) {
+            var roueActuelle = compteur.length - 1
+            while (roueActuelle >= 0) {
+                // Si la roue actuelle n'est pas à sa valeur max, l'incrémente
+                if (compteurCourant(roueActuelle) < compteur(roueActuelle)) {
+                    // Incrémente la roue actuelle
+                    compteurCourant(roueActuelle) += 1
+                    // Décrémente le nombre de valeurs restantes possibles
+                    compteurValRestantes -= 1
+                    return
+                } else {
+                    // Si la roue actuelle est à sa valeur max et qu'il y a d'autres roues à gauche,
+                    // met la roue actuelle à 0 et incrémente la roue à sa gauche.
+                    var roueGauche = roueActuelle - 1
+                    // Vérifie que la roue existe
+                    if (roueGauche >= 0) {
+                        // Si la roue à gauche n'est pas à sa valeur max, l'incrémente
+                        if (compteurCourant(roueGauche) < compteur(roueGauche)) {
+                            // remet à 0 tous les compteurs de droite
+                            for (j <- roueActuelle to compteur.length - 1) {
+                                compteurCourant(j) = 0
+                            }
+                            compteurCourant(roueGauche) += 1
+                            compteurValRestantes -= 1
+                            return
+                        } else {
+                            // Si la roue gauche est à sa valeur max, recommence l'opération
+                            // en avançant
+                            roueActuelle = roueGauche
+                        }
+                    }
+                }
+            }
+        }
+    }
+    
+    def suivantPossible(): Boolean = {
+        if (compteurCourant != null) {
+            compteurCourant.toList != compteur
+        } else false
+    }
+
+    def valPossibles(): Int = {
+        if (compteur != null) {
+            var res = 1
+            for (i <- 0 to compteur.length - 1) {
+                res = res * (compteur(i) + 1) // Ajout de 1 car commence à 0
+            }
+            res
+        } else 0
+    }
+
+    def valRestantes(): Int = {
+        compteurValRestantes
+    }
+
 }
--- GenieLogiciel/TP_TDD/tests/compteur/TestCompteur.scala
+++ GenieLogiciel/TP_TDD/tests/compteur/TestCompteur.scala
@@ -1,29 +1,142 @@
 package compteur
 
-import org.junit.Test
+import org.junit._
 import org.junit.Assert._
 
+/**
+ * Tests pour CompteurImplem.scala
+ */
 class TestCompteur {
-  val c: Compteur = new CompteurImpl()
+    /** Compteur utilisé pour les tests */
+    val c: Compteur = new CompteurImpl()
+    
+    /**
+     * Test1 : initialise un compteur comportant une roue pouvant avoir pour valeur 0 ou 1
+     * et réalise une série de tests sur ce compteur
+     */
+    @Test
+    def testCompteur1Roue() = {
+        // Initialise un compteur comportant une roue. Valeur actuelle 0, valeur max 1.
+        c.init(List(1))
+        // Doit rendre true (encore 1 valeur possible)
+        assertTrue(c.suivantPossible)
+        // Le compteur permet 2 valeurs possibles, 0 et 1
+        assertEquals(2, c.valPossibles)
+        // Le compteur actuel est List(0)
+        assertEquals(List(0), c.courant)
+        // Il reste 1 valeur possible à incrémenter
+        assertEquals(1, c.valRestantes)
+        
+        // Incrémente le compteur
+        c.suivant
+        
+        // Le compteur actuel est List(1)
+        assertEquals(List(1), c.courant)
+        // Le compteur est à sa valeur max
+        assertFalse(c.suivantPossible)
+        // Il reste 0 valeur possible à incrémenter
+        assertEquals(0, c.valRestantes)
+        
+        // Incrémente le compteur
+        // Pas de changement attendu car la valeur max du compteur est atteinte
+        c.suivant
+        
+        // Le compteur actuel est List(1)
+        assertEquals(List(1), c.courant)
+        // Le compteur est à sa valeur max
+        assertFalse(c.suivantPossible)
+        // Il reste 0 valeur possible à incrémenter
+        assertEquals(0, c.valRestantes)
+    }
 
-  @Test
-  def test1 {
-    c.init(List(1))
-    assertTrue(c.suivantPossible)
-    assertEquals(2, c.valPossibles)
-    assertEquals(List(0), c.courant)
-    assertTrue(c.suivantPossible)
-    c.suivant
-    assertEquals(List(1), c.courant)
-    assertFalse(c.suivantPossible)
-    assertEquals(c.valRestantes, 0)
-  }
-
-  @Test
-  def test2 {
-    c.init(List(1, 2, 3))
-    assertEquals(List(0, 0, 0), c.courant)
-    /*c.suivant
-    assertTrue(c.suivantPossible)*/
-  }
+    /**
+     * Test2 : initialise un compteur comportant 3 roues
+     * et réalise une série de tests sur ce compteur
+     */
+    @Test
+    def testCompteurPlusieursRoues() = {
+        // Initialise un compteur comportant 3 roues. Valeur actuelle 000, valeur max 123.
+        c.init(List(1, 2, 3))
+        // Le compteur permet 24 valeurs possibles
+        assertEquals(24, c.valPossibles)
+        // Doit rendre true (encore 23 valeurs possibles)
+        assertTrue(c.suivantPossible)
+        // Le compteur actuel est List(0, 0, 0)
+        assertEquals(List(0, 0, 0), c.courant)
+        // Il reste 23 valeurs possible à incrémenter
+        assertEquals(23, c.valRestantes)
+        
+        // Incrémente le compteur
+        c.suivant
+        
+        // Le compteur actuel est List(0, 0, 1)
+        assertEquals(List(0, 0, 1), c.courant)
+        // Doit rendre true (encore 22 valeurs possibles)
+        assertTrue(c.suivantPossible)
+        // Il reste 22 valeur possible à incrémenter
+        assertEquals(22, c.valRestantes)
+        
+        // Incrémente le compteur
+        c.suivant
+        
+        // Le compteur actuel est List(0, 0, 2)
+        assertEquals(List(0, 0, 2), c.courant)
+        // Doit rendre true (encore 21 valeurs possibles)
+        assertTrue(c.suivantPossible)
+        // Il reste 21 valeur possible à incrémenter
+        assertEquals(21, c.valRestantes)
+        
+        // Réalise 3 incrémentations
+        for(i <- 1 to 3) {
+            c.suivant
+        }
+        
+        // Le compteur actuel est List(0, 1, 1)
+        assertEquals(List(0, 1, 1), c.courant)
+        // Doit rendre true
+        assertTrue(c.suivantPossible)
+        // Il reste 18 valeur possible à incrémenter
+        assertEquals(18, c.valRestantes)
+        
+        // Réalise 18 incrémentations (jusqu'à la valeur max)
+        for(i <- 1 to 18) {
+            c.suivant
+        }
+        
+        // Le compteur actuel est la valeur max List(1, 2, 3)
+        assertEquals(List(1, 2, 3), c.courant)
+        // Doit rendre false
+        assertFalse(c.suivantPossible)
+        // Il reste 0 valeur possible à incrémenter
+        assertEquals(0, c.valRestantes)
+        
+        // Incrémente le compteur
+        // Le compteur ne doit pas s'incrémenter car il est à sa valeur max
+        c.suivant
+        
+        // Le compteur actuel est la valeur max List(1, 2, 3)
+        assertEquals(List(1, 2, 3), c.courant)
+        // Doit rendre false
+        assertFalse(c.suivantPossible)
+        // Il reste 0 valeur possible à incrémenter
+        assertEquals(0, c.valRestantes)
+    }
+    
+    /**
+     * Test1 : test de l'initialisation d'un compteur avec une liste vide
+     */
+    @Test
+    def testInitListeVide() = {
+        // Réalisation d'opérations avant l'initialisation
+        // Doivent rendre null ou 0 selon la méthode
+        assertEquals(null, c.courant())
+        assertEquals(0, c.valPossibles)
+        assertEquals(0, c.valRestantes)
+        assertFalse(c.suivantPossible)
+        // Initialisation d'un compteur avec une liste vide
+        // On choisit que l'initialisation d'une liste vide ne fait rien
+        c.init(Nil)
+        // Les compteur sont bien initialisés à null
+        assertEquals(null, c.courant())
+    }
 }
